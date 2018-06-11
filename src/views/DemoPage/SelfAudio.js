ddcd0c5d3a53cdd85937c8f015cbec6ab4918f2d
weiyu <y.wei@knowyourself.cc>
2018-06-11 20:31:40 +0800

    update

--- src/views/DemoPage/SelfAudio.js
+++ src/views/DemoPage/SelfAudio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./SelfAudio.scss";
+import audioIcon from "./images/audio.png";
 
 export class SelfAudio extends React.Component {
   constructor(props) {
@@ -21,9 +22,13 @@ export class SelfAudio extends React.Component {
     const audio = this.audio;
 
     if (audio && autoPlay) {
-      document.addEventListener("DOMContentLoaded", this.audioAutoPlay, { once: true });
+      document.addEventListener("DOMContentLoaded", this.audioAutoPlay, {
+        once: true
+      });
       //--创建触摸监听，当浏览器打开页面时，触摸屏幕触发事件，进行音频播放
-      document.addEventListener("touchstart", this.audioAutoPlay, { once: true });
+      document.addEventListener("touchstart", this.audioAutoPlay, {
+        once: true
+      });
     }
   }
 
@@ -35,26 +40,13 @@ export class SelfAudio extends React.Component {
     document.addEventListener("WeixinJSBridgeReady", () => audio.play());
   }
 
-  audioStatusChange() {
-    const { isPlay } = this.state;
-
-    this.audio[isPlay ? "pause" : "play"]();
-
-    this.setState({ isPlay: !isPlay });
-  }
-
   readyToPlay() {
-    const { autoPlay } = this.props;
     const audio = this.audio;
-    const { duration: allTime } = audio;
+    const { duration } = audio;
 
-    if (allTime !== 0) {
-      this.setState({ allTime });
-      // 是否自动播放
-      if (autoPlay) {
-        audio.play();
-        this.setState({ isPlay: !!autoPlay, isPlay: true });
-      }
+    if (audio.duration !== 0) {
+      audio.play();
+      this.setState({ isPlay: true });
     }
   }
 
@@ -64,132 +56,31 @@ export class SelfAudio extends React.Component {
     this.setState({ currentTime });
   }
 
-  /**
-   * format time
-   * @param {time} time
-   */
-  millisecondToDate(time) {
-    const second = Math.floor(time % 60);
-    const minite = Math.floor(time / 60);
-    // format: 00:00
-    return `${minite}:${second >= 10 ? second : `0${second}`}`;
-  }
-
-  // distance
-  handleTouchStart(e) {
-    const { frozen = false } = this.props;
+  clickToCheckPlayStatus() {
     const { isPlay } = this.state;
-    const { pageX: begin } = e.touches[0];
-    // 是否禁用滑动
-    if (frozen) return;
-    // 先让播放器暂停
-    if (isPlay) this.audio.pause();
-    // 存入滑动状态
-    this.setState({ begin });
-  }
 
-  handleTouchMove(e) {
-    const { begin, allTime } = this.state;
-    const { pageX } = e.touches[0];
-    const { width: maxRange, left } = this.maxRangeEl.getBoundingClientRect();
+    this.audio[isPlay ? "pause" : "play"]();
 
-    if (begin !== 0) {
-      // 滑动的距离
-      const distance = pageX - left;
-      const target = allTime * (distance / maxRange);
-      // 考虑超出的情况
-      if (target > allTime) {
-        this.audio.currentTime = allTime;
-
-        this.setState({ currentTime: allTime });
-      } else if (target < 0) {
-        this.audio.currentTime = 0;
-
-        this.setState({ currentTime: 0 });
-      } else {
-        this.audio.currentTime = target;
-
-        this.setState({ currentTime: target });
-      }
-    }
-  }
-
-  handleTouchEnd(e) {
-    const audio = this.audio;
-    const { isPlay } = this.state;
-    // console.log(isPlay);
-
-    if (isPlay) audio.play();
-
-    this.setState({ begin: 0 });
-  }
-
-  jumpToVoice(e) {
-    const { jump = true } = this.props;
-    // 是否跳转
-    if (!jump) return;
-
-    const { allTime } = this.state;
-    const { pageX, target } = e.touches[0];
-    const { width, left } = this.maxRangeEl.getBoundingClientRect();
-    // console.log(pageX, left, width)
-    const currentTime = ((pageX - left) / width) * allTime;
-
-    this.audio.currentTime = currentTime;
-    this.setState({ currentTime });
+    this.setState({ isPlay: !isPlay });
   }
 
   render() {
-    const {
-      source = "http://65.ierge.cn/13/206/412922.mp3",
-      poster = "https://kypublic.oss-cn-shanghai.aliyuncs.com/2018/04/20/47/15242171412329EPilu.jpeg",
-      name = "测试音频"
-    } = this.props;
-    const { currentTime, allTime, isPlay } = this.state;
-
-    const startTime = this.millisecondToDate(currentTime);
-    const endTime = this.millisecondToDate(allTime);
-
-    const currentRange = `${
-      currentTime > 0 ? (currentTime / allTime) * 100 : 0
-    }%`;
+    const { source = "http://65.ierge.cn/13/206/412922.mp3" } = this.props;
+    const { isPlay } = this.state;
 
     return (
       <div className="self-audio-play">
-        <div className="poster" style={{ backgroundImage: `url(${poster})` }}>
-          <div className="play" onClick={this.audioStatusChange.bind(this)}>
-            {isPlay ? "暂停" : "播放"}
-          </div>
-        </div>
+        <div
+          className={`control ${isPlay ? "self-rotate" : ""}`}
+          style={{ backgroundImage: `url(${audioIcon})` }}
+          onClick={this.clickToCheckPlayStatus.bind(this)}
+        />
         <div className="audio-wrap">
           <audio
-            id="audio-source"
             onCanPlay={this.readyToPlay.bind(this)}
-            onTimeUpdate={this.getCurrentPlayTime.bind(this)}
             ref={audio => (this.audio = audio)}
             src={source}
           />
-          <div className="controls">
-            <div className="title">{name}</div>
-            <div
-              className="range-wrap"
-              onTouchStart={this.jumpToVoice.bind(this)}
-              ref={el => (this.maxRangeEl = el)}
-            >
-              <div className="range" style={{ width: currentRange }}>
-                <span
-                  className="sign"
-                  onTouchStart={this.handleTouchStart.bind(this)}
-                  onTouchMove={this.handleTouchMove.bind(this)}
-                  onTouchEnd={this.handleTouchEnd.bind(this)}
-                />
-              </div>
-            </div>
-            <div className="times-show">
-              <span className="start">{startTime}</span>
-              <span className="end">{endTime}</span>
-            </div>
-          </div>
         </div>
       </div>
     );
@@ -197,12 +88,7 @@ export class SelfAudio extends React.Component {
 }
 
 SelfAudio.propTypes = {
-  source: PropTypes.string,
-  poster: PropTypes.string,
-  name: PropTypes.string,
-  jump: PropTypes.bool,
-  frozen: PropTypes.bool,
-  autoPlay: PropTypes.bool
+  source: PropTypes.string
 };
 
 export default SelfAudio;
